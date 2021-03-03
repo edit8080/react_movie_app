c35f8a3899a618cec77cb81de64cb35a1d1a25f8
edit8080 <tlfla01@gmail.com>
2021-03-03 14:48:39 +0900

    Redirecting
    - state가 없는 상태면 home으로 redirecting

--- src/App.js
+++ src/App.js
@@ -2,8 +2,8 @@ import React from "react";
 import { HashRouter, Route } from "react-router-dom";
 import Home from "./routes/Home";
 import About from "./routes/About";
-import Navigation from "./components/Navigation";
 import Detail from "./routes/Detail";
+import Navigation from "./components/Navigation";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Navigation />
       <Route path="/" exact={true} component={Home} />
       <Route path="/about" component={About} />
-      <Route path="/movie-detail" component={Detail} />
+      <Route path="/movie/:id" component={Detail} />
     </HashRouter>
   );
 }
